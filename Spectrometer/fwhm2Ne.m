a2ef36523a8a6c0bfb894120e342cbbfbc24aa85
etan <40403567+etanx@users.noreply.github.com>
2019-05-16 17:10:47 -0500

    Added questions where uncertain

--- Spectrometer/fwhm2Ne.m
+++ Spectrometer/fwhm2Ne.m
@@ -1,9 +1,13 @@
-function [ Ne ] = fwhm2Ne( fwhm_raw )
+function [ n_e ] = fwhm2ne( fwhm_raw )
 %Converting the fwhm quickly to density
 %   hydrogenlamp
-hydrogenLamp = 5.76;
+hydrogenLamp = 5.76; % this must be some calibration factor? - Ellie Tan
 
-Ne =  1e20 * ((( 0.014 * sqrt(  fwhm_raw.^2 - hydrogenLamp^2  ) ) ./ 0.04).^(3/2))
+n_e =  1e20 * ((( 0.014 * sqrt(  fwhm_raw.^2 - hydrogenLamp^2  ) ) ./ 0.04).^(3/2))
+
+% where did values 0.014 and 0.04 come from? 3/2?
+% is the fwhm in units of pixels of nm? 
+% Ellie Tan.
 
 end
 
