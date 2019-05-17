08db5873e89012cb43dc276caa3a3768a822c2ab
etan <40403567+etanx@users.noreply.github.com>
2019-05-17 12:20:44 -0500

    New function to compile all extracted velocities

---
+++ PPDimaging/PPDvelDistribution.m
@@ -0,0 +1,42 @@
+function [v, v_avg, dv_avg] = PPDvelDistribution( folderpath )
+
+% script combines all the extracted velocities in a selected folder. Velocities must be
+% first extracted and saved with PPD2vel.m, PPDvelExtract2, or equiv. and
+% start with 'vel' in the filename.
+
+% Elizabeth H. Tan, 17 May 2019.
+
+% example: 
+% [v, v_avg, dv_avg] = PPDvelDistribution('C:\Users\Plasma\Box\elizabethtan\2018-19 WIRX Honors Thesis\Code\Argon')
+
+% navigate to location of top folder containing velocity data
+FileList = dir([folderpath '\vel*.mat']); % list all files with pattern
+
+%create empty variable arrays
+velocities = []; 
+v_avg = [];
+dv_avg = [];
+
+% loop through all files in list
+for i = 1:length(FileList)
+
+filepath = [FileList(i).folder '\' FileList(i).name];    
+clear vel
+load(filepath)
+% average all events in one shot
+v_avg(i) = mean(vel);
+dv_avg(i) = std(vel./1e5)./sqrt(length(FileList));
+
+% FUTURE: Compiple list of shots and other parameters from all the files too!
+
+% combine new data into array
+velocities = cat(1,velocities,vel);
+
+end
+
+% Arrange in increasing order
+v = sort(velocities);
+v_avg = sort(abs(v_avg./100))';
+dv_avg = abs(dv_avg)';
+
+
