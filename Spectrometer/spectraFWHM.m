31d669e466683b05535de2e27c9bf7cb20035572
etan <40403567+etanx@users.noreply.github.com>
2019-05-21 16:46:56 -0500

    Added comments

--- Spectrometer/spectraFWHM.m
+++ Spectrometer/spectraFWHM.m
@@ -4,6 +4,8 @@ function [ width_px, width_nm ] = spectraFWHM( firstSpectraPaths )
 % Gets FWHM at all costs
 numImages = length(firstSpectraPaths);
 width_px = zeros(1,numImages,1);
+
+
 for i = 1:numImages
     imageData = imread(firstSpectraPaths{i},'tiff');
     raw_spectra = mean(imageData(369:725,:)); %Finds the meain intensity of middle of vertical line I think? -Ellie
@@ -15,6 +17,7 @@ for i = 1:numImages
     
     % According to Dr Craig's notes, 1 pixel is about 0.02 cm for the spectra images.
     % Probably need to verify this somehow. - Ellie
+    % EDIT: This value is incorrect for spectrometer.
 end
 end
 
