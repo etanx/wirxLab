bf01313737f7a926d764f5fcefc82ac93cd51772
jodiemclennan <45829434+jodiemclennan@users.noreply.github.com>
2019-05-23 13:45:40 -0500

    Update calibrate.m

--- Spectrometer/calibrate.m
+++ Spectrometer/calibrate.m
@@ -39,29 +39,6 @@ HImgData = Himage;
 HeImgData = Heimage;
 ArImgData = ArImage;
 
-% %% DISPLAY RAW SPECTRA IMAGE (Denisty.m code, probably not needed)
-% % show original greyscale figure
-% figure
-% fig = imagesc(HImgData); 
-% colormap 'gray'; %Convert figure into a RGB 'jet' or grayscale 'gray' image.
-% set(gca, 'Visible', 'off')
-% %text(5,40,num2str(shot),'Color','white') % label shot number on image
-% 
-% % Show 3D figure
-% height = 1:1:size(HImgData,1);
-% width = 1:1:size(HImgData,2);
-% figure;
-% [X, Y] = meshgrid(width, height);
-% Z = HImgData;
-% fig = surf(X,Y,Z);
-% colormap 'jet'; %Use 'jet' for more interesting looking pictures.
-% set(fig, 'EdgeColor', 'none');
-% xlabel('Width (px)')
-% ylabel('Height (px)')
-% zlabel('Intensity')
-% title('Raw Image')
-% 
-
 %% CLEAN UP DATA (three times, once for each image. Future: make this a loop?)
 % Find background average minus the peak
 % disp('Estimating background...')
@@ -96,7 +73,69 @@ ArImageOffset = ArImgData - ArImgBackground;
 % combined image using all three spectra
 combImage = HImgOffset + HeImageOffset + ArImageOffset;
 
-% %% Show 3D figure without background offset subtracted
+%% Average intensity vertically for height of line
+img_avg = mean( combImage(lineHeight,:),1 ); % take vertical average of the line.
+
+% Plot 1D average intensity
+figure;
+plot(img_avg)
+xlabel('Width (pixels)')
+ylabel('Line-Averaged Intensity')
+grid on
+
+
+%% Find Peaks
+pixels = 1:length(img_avg);
+[peakInten, peakPos] = findpeaks(img_avg,pixels,'MinPeakHeight',threshold,...
+'SortStr',descend,'NPeaks',5); 
+peakPos = sort(peakPos);
+
+
+
+%% CONVERSION: Pixels to nanometers
+
+switch grating
+    case 150
+        Hdistnm = 486-434;
+        Hdistpix = peakPos(3)-peakPos(1);
+        a1 = Hdistnm/Hdistpix;
+        HeDist12nm = 588-501;
+        HeDist12pix = peakPos(5)-peakPos(4);
+        HeDist23nm = 501-447;
+        HeDist23pix = peakPos(4)-peakPos(2);
+        He12 = HeDist12nm/HeDist12pix;
+        He23 = HeDist23nm/HeDist23pix;
+        a = 1/3*(He12+He23+a1);
+    case 1800
+        distnm = 486-492;
+        distpix = peakPos(2)-peakPos(1);
+        a = distnm/distpix;
+end
+
+%% DISPLAY RAW SPECTRA IMAGE (Denisty.m code, probably not needed)
+% % show original greyscale figure
+% figure
+% fig = imagesc(HImgData); 
+% colormap 'gray'; %Convert figure into a RGB 'jet' or grayscale 'gray' image.
+% set(gca, 'Visible', 'off')
+% %text(5,40,num2str(shot),'Color','white') % label shot number on image
+% 
+% % Show 3D figure
+% height = 1:1:size(HImgData,1);
+% width = 1:1:size(HImgData,2);
+% figure;
+% [X, Y] = meshgrid(width, height);
+% Z = HImgData;
+% fig = surf(X,Y,Z);
+% colormap 'jet'; %Use 'jet' for more interesting looking pictures.
+% set(fig, 'EdgeColor', 'none');
+% xlabel('Width (px)')
+% ylabel('Height (px)')
+% zlabel('Intensity')
+% title('Raw Image')
+% 
+
+% Show 3D figure without background offset subtracted
 % height = 1:1:size(imgOffset,1);
 % width = 1:1:size(imgOffset,2);
 % figure;
@@ -110,50 +149,12 @@ combImage = HImgOffset + HeImageOffset + ArImageOffset;
 % zlabel('Intensity')
 % title('Background-subtracted')
 
-%% Average intensity vertically for height of line
-img_avg = mean( combImage(lineHeight,:),1 ); % take vertical average of the line.
-
-% Plot 1D average intensity
-figure;
-plot(img_avg)
-xlabel('Width (pixels)')
-ylabel('Line-Averaged Intensity')
-grid on
-
-
-%%There is an optional section you can include here. Code at the end of script.
+%There is an optional section you can include here. Code at the end of script.
 %It can clean up data, but is difficult to deal with multiple peaks!
 
-%% Find Peaks
-pixels = 1:length(img_avg);
-[peakInten, peakPos] = findpeaks(img_avg,pixels,'MinPeakHeight',threshold,...
-'SortStr',descend,'NPeaks',5); 
-
-
-
-%% get FWHM from spectra for density if there's a Hbeta line
-
+% get FWHM from spectra for density if there's a Hbeta line
 
 %intensity = img_avg;
 %fwhmPixels = fwhm(pixels,intensity); 
 % ^check if this is actualyl correct since I didn't crop to the peak region only
 
-
-%% CONVERSION: Pixels to nanometers
-
-switch grating
-    case 150
-        peakPos = sort(peakPos);
-        Hdistnm = 486-434;
-        Hdistpix = peakPos(3)-peakPos(1);
-        a1 = Hdistnm/Hdistpix;
-        HeDist12nm = 588-501;
-        HeDist12pix = peakPos(5)-peakPos(4);
-        HeDist23nm = 501-447;
-        HeDist23pix = peakPos(4)-peakPos(2);
-        He12 = HeDist12nm/HeDist12pix;
-        He23 = HeDist23nm/HeDist23pix;
-        a = 1/3*(He12+He23+a1);
-    case 1800
-
-end
